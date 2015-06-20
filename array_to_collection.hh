3f517d444229b3288a94f45f87cd84745da0e3ce
Travis Brown <tmbrown6@gmail.com>
2015-06-20 00:11:11 -0400

    Create array_to_collection.hh

---
+++ array_to_collection.hh
@@ -0,0 +1,23 @@
+<?hh
+
+// Include the required functions
+require_once('array_to_map.hh');
+require_once('array_to_vector.hh');
+
+/**
+ * This method converts an array to a collection type of either a Vector or a Map
+ * @name array_to_collection()
+ * @param array<mixed, mixed> $arrSource
+ * @return Map<string, mixed>|Vector<mixed>
+ */
+function array_to_collection(array $arrSource) : mixed
+{
+	// Check for an associative array
+	if (array_is_associative($arrSource) === true) {
+		// Return the data
+		return array_to_map($arrSource);
+	} else {
+		// Return the data
+		return array_to_vector($arrSource);
+	}
+}
